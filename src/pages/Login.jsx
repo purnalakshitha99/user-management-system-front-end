d6fcbeb51ec7e4bfe579539cc33c5059d5bb4006
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2025-08-27 17:42:28 +0530

    div box get to center

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,9 +2,18 @@ import React from "react";
 
 function Login() {
   return (
-    <h1 className="font-bold text-center mt-10  text-2xl text-blue-600">
+    <>
+    <h1 className="font-bold text-center mt-20  text-3xl text-blue-600">
       Welcome Back
     </h1>
+    <p class="text-center">Sign in to your account</p>
+    <center>
+  <div className="flex justify-center mt-20 shadow-xl/30 p-4 bg-amber-400  w-80  h-80 shadow-xl  ">
+    asdasd
+  </div>
+  </center>
+   
+    </>
   );
 }
 
