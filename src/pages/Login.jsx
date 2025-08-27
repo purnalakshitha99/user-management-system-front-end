487f3b440f796bca148589b96c3b5502c0e1a3a1
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2025-08-27 22:11:30 +0530

    added new bg colours and rectangle buttons

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,12 +4,12 @@ function Login() {
   return (
     <>
     <h1 className="font-bold text-center mt-20  text-3xl text-blue-600">
-      Welcome Back
+     <div>Welcome Back</div>
     </h1>
     <p class="text-center">Sign in to your account</p>
     <center>
-  <div className="flex justify-center mt-20 shadow-xl/30 p-4 bg-amber-400  w-80  h-80 shadow-xl  ">
-    asdasd
+  <div className="flex justify-center bg-white mt-20 shadow-xl/30 p-4  w-150  h-150 rounded-2xl shadow-xl  ">
+    <div className="flex bg-sky-100 border border-indigo-400 w-130 h-20 rounded-2xl mt-5"></div>
   </div>
   </center>
    
