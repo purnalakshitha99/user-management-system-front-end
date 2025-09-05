7ee72409a4893edbf76ba789f067778ca4db36c6
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2025-09-05 20:39:17 +0530

    registration form link to app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Login from './pages/Login'
 import Example from './pages/InlineVsBlock'
+import Register from './pages/Register'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import './App.css'
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/example" element={<Example />} />
+        <Route path="/Register" element={<Register />} />
       </Routes>
     </Router>
   )
