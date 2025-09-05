b9c80e846a88fcf9a10e39ddb6c5ce488aafedfb
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2025-09-05 17:19:05 +0530

    hyperlink add for sign up text

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -18,6 +18,10 @@ function Login() {
           </label>
           <input type="password" placeholder="enter the email" className="px-4 py-2 w-full border border-gray-300 rounded-lg  focus:outline-none focus:ring-2 focus:ring-blue-500"></input>
           </div>
+          <div>
+            <button type="submit" className="w-full bg-blue-500 text-white rounded-lg py-2 mt-2 font-semibold" >Login</button>
+          </div>
+          <p className="text-center text-sm">Don't have an account? <a href="#" className="text-blue-500 hover:underline">Sign Up</a> </p>
         </form>
       </div>
     </div>
