a22a692b0d9de3911b045e35236b0e1fceba5634
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2025-09-05 17:00:38 +0530

    new div add for password

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,11 +12,12 @@ function Login() {
           </label>
           <input type="email" placeholder="enter the email" className="w-full px-4 py-2 border border-gray-300 rounded-lg  mb-5 focus:ring-2 focus:outline-none focus:ring-blue-500"></input>
           </div>
-          {/* <input type="email" placeholder="enter the email" className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"></input> */}
+          <div>
           <label className="block text-gray-600 text-[16px] mb-1">
             Password
           </label>
           <input type="password" placeholder="enter the email" className="px-4 py-2 w-full border border-gray-300 rounded-lg  focus:outline-none focus:ring-2 focus:ring-blue-500"></input>
+          </div>
         </form>
       </div>
     </div>
